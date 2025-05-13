ba0a71aea810a929e06740c0540928975a6bc10b
kitabmantra <kitabmantra08@gmail.com>
2025-05-13 15:06:04 +0000

    recording changes

--- src/components/QuillEditor.tsx
+++ src/components/QuillEditor.tsx
@@ -2,44 +2,59 @@
 
 import { useCallback, useEffect, useRef, useState } from "react";
 import "quill/dist/quill.snow.css";
+import { Delta } from "quill";
 
 const TOOLBAR_OPTIONS = [
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],
-  [{ font: [] }],
-  [{ list: "ordered" }, { list: "bullet" }],
-  [{ color: [] }, { background: [] }],
-  ["bold", "italic", "underline"],
-  [{ script: "sub" }, { script: "super" }],
-  [{ align: [] }],
-  ["image", "blockquote", "code-block"],
-  ["clean"],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ color: [] }, { background: [] }],
+    ["bold", "italic", "underline"],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
 ];
 
 export default function QuilEditor() {
-  const wrapperRef = useRef<HTMLDivElement | null>(null);
-  const [quill, setQuill] = useState<any>(null);
+    const wrapperRef = useRef<HTMLDivElement | null>(null);
+    const [quill, setQuill] = useState<any>(null);
 
-  const setupEditor = useCallback(async () => {
-    const Quill = (await import("quill")).default;
+    const setupEditor = useCallback(async () => {
+        const Quill = (await import("quill")).default;
 
-    if (wrapperRef.current) {
-      wrapperRef.current.innerHTML = "";
-      const editor = document.createElement("div");
-      wrapperRef.current.append(editor);
+        if (wrapperRef.current) {
+            wrapperRef.current.innerHTML = "";
+            const editor = document.createElement("div");
+            wrapperRef.current.append(editor);
 
-      const q = new Quill(editor, {
-        theme: "snow",
-        modules: {
-          toolbar: TOOLBAR_OPTIONS,
-        },
-      });
-      setQuill(q);
-    }
-  }, []);
+            const q = new Quill(editor, {
+                theme: "snow",
+                modules: {
+                    toolbar: TOOLBAR_OPTIONS,
+                },
+            });
+            setQuill(q);
+        }
+    }, []);
+    useEffect(() => {
+        if (!quill) return;
 
-  useEffect(() => {
-    setupEditor();
-  }, [setupEditor]);
+        const handler = (delta: any, oldDelta: any, source: string) => {
+            if (source !== "user") return;
+            console.log(delta);
+        };
 
-  return <div className="container" ref={wrapperRef} />;
+        quill.on("text-change", handler);
+
+        return () => {
+            quill.off("text-change", handler);
+        };
+    }, [quill]);
+
+    useEffect(() => {
+        setupEditor();
+    }, [setupEditor]);
+
+    return <div className="container" ref={wrapperRef} />;
 }
